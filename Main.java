c3effa614c07b89c55d9d866d956b0e12cce07c7
fatimatuzzahra002 <91249354+fatimatuzzahra002@users.noreply.github.com>
2021-10-22 17:59:36 +0800

    Java-Challenge-5

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+    int x;
+    int y;
+
+    public static void main(String[] args) {
+        Hero hero = new Hero("Zahra", 21);
+        hero.player();
+        looping(2, () -> hero.walk());
+        looping(3, () -> hero.run());
+        looping(5, () -> hero.eat());
+        looping(2, () -> hero.drink());
+        looping(50, () -> hero.walk());
+        looping(6, () -> hero.jump());
+        looping(5, () -> hero.sit());
+        looping(3, () -> hero.attack());
+        hero.player();
+    }
+
+    static void looping(int x, Runnable r) {
+        for (int i = 0; i < x; i++) {
+            r.run();
+        }
+    }
+}
